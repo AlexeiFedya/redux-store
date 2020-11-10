36bfc6e7872961153aabbe07a201e5bd0d54c277
AlexeiFedya <fedya5711@icloud.com>
2020-11-10 20:34:28 +0300

    Updates

---
+++ precache-manifest.f0df459496f896b4cd0f4ff56073a6d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9843c9eec80056a0bddf7f8488023ea6",
+    "url": "/redux-store/index.html"
+  },
+  {
+    "revision": "ae84d0b03498e762c8b8",
+    "url": "/redux-store/static/css/main.a4976c57.chunk.css"
+  },
+  {
+    "revision": "8759c7ae0322680ee07a",
+    "url": "/redux-store/static/js/2.f071edb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-store/static/js/2.f071edb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae84d0b03498e762c8b8",
+    "url": "/redux-store/static/js/main.270769a3.chunk.js"
+  },
+  {
+    "revision": "1d7bc121dfb882ce1ee5",
+    "url": "/redux-store/static/js/runtime-main.dd12185c.js"
+  }
+]);
